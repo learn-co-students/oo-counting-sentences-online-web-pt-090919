d9aa7032469633661658af46b8af881205135e6f
Brianna Jordan <bryvjordan9@gmail.com>
2019-10-01 23:46:16 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,8 +3,10 @@ require 'pry'
 class String
 
   def sentence?
-    
+    puts "Hi, my name is Sophie."
+    "Hi, my name is Sophie.".end_with?("Hi, my name is Sophie.", "Hi, my name is Sophie")
   end
+end
 
   def question?
 
